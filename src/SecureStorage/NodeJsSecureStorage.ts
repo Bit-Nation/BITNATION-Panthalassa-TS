cedd7c7bc71963e44da599bb7c5d0686d9250db1
Alexander Bolshakov <lextiz@gmail.com>
2017-09-29 01:37:58 +0300

    Tests

--- src/SecureStorage/NodeJsSecureStorage.ts
+++ src/SecureStorage/NodeJsSecureStorage.ts
@@ -1,15 +1,16 @@
 import {SecureStorageInterface,SecureStorageManagerInterface} from './SecureStorageInterface';
-import {randomBytes,createCipheriv,createDecipheriv, createHash} from 'crypto';
 import {readFileSync,writeFileSync, unlinkSync, statSync} from 'fs';
 //import {Cryptr} from 'cryptr';
 var Cryptr = require('cryptr');
 export class NodeJsSecureStorage implements SecureStorageInterface {
     fileName: string;
     keyValueStore: Map<string,any>;
+    password: string;
     cryptr: Cryptr;
     constructor (fileName: string, password: string) {
         this.cryptr = new Cryptr(password);        
         this.fileName = fileName;
+        this.password = password;
         try {
             statSync(fileName);
             this.keyValueStore = JSON.parse(this.cryptr.decrypt(readFileSync(fileName, "utf8")));
@@ -18,7 +19,7 @@ export class NodeJsSecureStorage implements SecureStorageInterface {
         }
     }
     async getItem(key: string): Promise<any> {
-        this.keyValueStore.get(key);
+        return this.keyValueStore.get(key);
     }
     async setItem(key: string, value: any) {
         this.keyValueStore.set(key, value);
@@ -29,8 +30,12 @@ export class NodeJsSecureStorage implements SecureStorageInterface {
         await this.store();
     }
 
-    private async store() {
+    async store() {
         writeFileSync(this.fileName, this.cryptr.encrypt(JSON.stringify(this.keyValueStore)));
+    }
+
+    isPasswordValid (password:string) : boolean {
+        return this.password === password;
     }
 }
 
@@ -38,22 +43,28 @@ export class NodeJsSecureStorageManager implements SecureStorageManagerInterface
 
     openSessions: Map<string, SecureStorageInterface> = new Map<string, SecureStorageInterface>();
 
-    async openOrCreateStorage(name: string, password: string): Promise < SecureStorageInterface> {
+    async openOrCreateStorage(name: string, password: string): Promise <SecureStorageInterface> {
         // Authentication is handled by mobile OS
-        if (!this.openSessions.has(name)) {
-            this.openSessions.set(name, new NodeJsSecureStorage(name, password));
+        let session = this.openSessions.get(name);
+        if (!session) {
+            let newSession = new NodeJsSecureStorage(name, password);
+            this.openSessions.set(name, newSession);
+            newSession.store();
+            return newSession;
+        } else {
+            if (session.isPasswordValid(password)) {
+                // We only return storage file if it can be decrypted
+                return session;
+            } else {
+                throw new Error("Cannot read, decrypt or parse the storage");
+            }
         }
-        return this.openSessions.get(name);
     }
     async deleteStorage(name: string, password: string): Promise <void> {
         // Not implemented in react-native-sensitive-info
         // Instead of deleting the secure storage we will delete all its content
         let storage = await this.openOrCreateStorage(name, password);
-        if (storage) {
-            // We only delete the file if it can be decrypted
-            unlinkSync(name);
-        } else {
-            throw new Error("Cannot read, decrypt or parse the storage");
-        }
+        unlinkSync(name);
+        this.openSessions.delete(name);
     }
 }
--- test/SecureStorage/NodeJsSecureStorage.spec.ts
+++ test/SecureStorage/NodeJsSecureStorage.spec.ts
@@ -1,5 +1,6 @@
 import { statSync, unlinkSync } from 'fs';
 import { NodeJsSecureStorage, NodeJsSecureStorageManager } from '../../src/SecureStorage/NodeJsSecureStorage';
+import {SecureStorageInterface} from '../../src/SecureStorage/SecureStorageInterface'
 
 describe('secure storage for nodejs platform', () => {
 
@@ -7,18 +8,61 @@ describe('secure storage for nodejs platform', () => {
     let testStoragePassword = "assword";
 
     test('should create file for non existing storage', async () => {
+        await createTestStorageAndAssertItExists();
+    });
+
+    test('should store string value and able to retrive it', async () => {
+        let { storageManager, storage } = await createTestStorageAndAssertItExists();
+        storeValueRetriveAndAssertIt("key1","value1", storage);
+    });
+
+    test('should store boolean value and able to retrive it', async () => {
+        let { storageManager, storage } = await createTestStorageAndAssertItExists();
+        storeValueRetriveAndAssertIt("key1",true, storage);
+    });
+
+    test('should store multiple string values and able to retrive it', async () => {
+        let { storageManager, storage } = await createTestStorageAndAssertItExists();
+        for (let i = 0; i < 20; i++) {
+            storeValueRetriveAndAssertIt("key" + i,"value" + i, storage);
+        }
+        expect(await storage.getItem("key0")).toBe("value0");
+    });
+
+    test('should remove file after deletion of storage, when suppied right password', async () => {
+        let { storageManager, storage } = await createTestStorageAndAssertItExists();
+        await storageManager.deleteStorage(testStorageName, testStoragePassword);
+        expect(doesFileExist(testStorageName)).toBe(false);
+    });
+
+    test('should not remove file after deletion of storage, when suppied wrong password', async () => {
+        let { storageManager, storage } = await createTestStorageAndAssertItExists();
+        try {
+            await storageManager.deleteStorage(testStorageName, "wrongPassword");
+            expect(false).toBe(true);
+        } catch (e) {
+            expect(doesFileExist(testStorageName)).toBe(true);
+        }
+    });
+
+    afterEach(() => {
+        try {
+            unlinkSync(testStorageName);
+        } catch (e) {}
+    });
+
+    async function createTestStorageAndAssertItExists() {
         let storageManager = new NodeJsSecureStorageManager();
         expect(doesFileExist(testStorageName)).toBe(false);
         let storage = await storageManager.openOrCreateStorage(testStorageName, testStoragePassword);
         expect(doesFileExist(testStorageName)).toBe(true);
-        await storage.setItem("key1","value1");
-        expect(await storage.getItem("key1")).toBe("value1");
-        storageManager.deleteStorage(testStorageName, testStoragePassword);
-    });
+        return { storageManager, storage};
+    }
 
-    afterEach(() => {
-        unlinkSync(testStorageName);
-    });
+    async function storeValueRetriveAndAssertIt(key:string, value:any, storage: SecureStorageInterface) {
+        await storage.setItem(key,value);
+        expect(await storage.getItem(key)).toBe(value);
+    }
 
 });
 
