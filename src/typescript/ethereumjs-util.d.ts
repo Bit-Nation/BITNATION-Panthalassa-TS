ffa331b20ae25a25172c2313c6d792a7a4e783c2
Florian Lenz <flenz81@googlemail.com>
2017-10-01 17:32:58 +0200

    [typings] added isValidPrivate, privateToPublic, isValidPublic to ethereumjs-util

--- src/typescript/ethereumjs-util.d.ts
+++ src/typescript/ethereumjs-util.d.ts
@@ -1,5 +1,23 @@
 declare module 'ethereumjs-util' {
 
-    export function isValidPrivate(privateKey: string) : boolean;
+    /**
+     *
+     * @param {string} privateKey
+     * @returns {boolean}
+     */
+    export function isValidPrivate(privateKey:string) : boolean;
 
+    /**
+     *
+     * @param {string} privateKey
+     * @returns {string}
+     */
+    export function privateToPublic(privateKey:string) : string;
+
+    /**
+     *
+     * @param {string} pubKey
+     * @returns {string}
+     */
+    export function isValidPublic(pubKey:string) : string;
 }
