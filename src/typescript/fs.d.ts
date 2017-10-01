78c136c3d854d6bd7981b29614a801558be70727
Florian Lenz <flenz81@googlemail.com>
2017-10-01 10:14:11 +0200

    Revert "[typings] added fs - copyFileSync method"
    This reverts commit eb89601f50342bbc0ed0c5fea64c3a21ba275057.

--- src/typescript/fs.d.ts
+++
@@ -1,6 +0,0 @@
-declare module 'fs' {
-
-    //@todo this is a tmp fix for: https://github.com/DefinitelyTyped/DefinitelyTyped/pull/20157
-    export function copyFileSync(src:string, dest:string, flags?:number);
-
-}
