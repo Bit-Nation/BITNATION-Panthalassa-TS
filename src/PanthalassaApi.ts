d05ea8fed86c9fd06f9d42176d278b716aaae1d9
Florian Lenz <flenz81@googlemail.com>
2017-10-07 17:00:43 +0200

    [panthalassa-api] removed dependency on node js file system

--- src/PanthalassaApi.ts
+++ src/PanthalassaApi.ts
@@ -1,12 +1,8 @@
 import Repo from './Repo';
 import FileSystemInterface from 'BITNATION-Panthalassa-TS-fs-interface/FileSystemInterface';
 import {EthUtils} from "./EthWallet/EthUtils";
-import {SecureStorageInterface} from "./SecureStorage/SecureStorageInterface";
 import PrivateKey from "./EthWallet/PrivateKey";
-import {NodeJsSecureStorage, NodeJsSecureStorageManager} from "./SecureStorage/NodeJsSecureStorage";
-import {NodeJsFs} from "./FileSystem/NodeJsFs";
-import Path = require('path');
-import {ReactNativeSecureStorageManager} from './SecureStorage/ReactNativeSecureStorage'
+import {SecureStorageInterface} from "./SecureStorage/SecureStorageInterface";
 
 export class PanthalassaApi
 {
@@ -20,44 +16,17 @@ export class PanthalassaApi
 
 }
 
+/**
+ *
+ * @param {FileSystemInterface} fs
+ * @param {SecureStorageInterface} secStorage
+ * @returns {PanthalassaApi}
+ */
+export function factory(fs:FileSystemInterface, secStorage:SecureStorageInterface){
 
-export function factory(platform:string, repoPath:string, secureStoragePassword:string){
-
-    let ss:Promise<SecureStorageInterface>;
-    let fs:FileSystemInterface;
-
-    switch (platform){
-
-        case 'ios' : {
-            //Todo prove this before implementing in app
-            let storageManager:ReactNativeSecureStorageManager = new ReactNativeSecureStorageManager();
-            ss = storageManager.createStorage('bitn_secure_storage');
-            break;
-        }
-        case 'android': {
-            //Todo prove this before implementing in app
-            let storageManager:ReactNativeSecureStorageManager = new ReactNativeSecureStorageManager();
-            ss = storageManager.createStorage('bitn_secure_storage');
-            break;
-        }
-        case 'node' : {
-            let storageManager:NodeJsSecureStorageManager = new NodeJsSecureStorageManager();
-            ss = storageManager.createStorage('node_js_secure_storage', secureStoragePassword);
-            fs = new NodeJsFs(repoPath, Path);
-            break;
-        }
-        default : {
-            throw new Error();
-        }
-
-    }
-
-    ss.then((storage:SecureStorageInterface) => {
-
-        const ethUtils = new EthUtils(storage, PrivateKey);
-
-        return new PanthalassaApi(new Repo(fs, ethUtils), new EthUtils(storage, PrivateKey));
-
-    });
+    return new PanthalassaApi(
+        new Repo(fs, new EthUtils(secStorage, PrivateKey)),
+        new EthUtils(secStorage, PrivateKey)
+    );
 
 }
