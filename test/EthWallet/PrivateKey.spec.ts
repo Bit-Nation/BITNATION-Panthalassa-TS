f12b19d023f736652abb3ab712e81d779ff41dee
Florian Lenz <flenz81@googlemail.com>
2017-10-09 16:10:22 +0200

    [tests] added test for private key thrown error

--- test/EthWallet/PrivateKey.spec.ts
+++ test/EthWallet/PrivateKey.spec.ts
@@ -1,5 +1,6 @@
 import PrivateKey from './../../src/EthWallet/PrivateKey';
 import {isValidPrivate} from 'ethereumjs-util';
+import {InvalidPrivateKey} from "../../src/Errors";
 
 // TODO: Coverage is still not reported correctly. See: https://stackoverflow.com/questions/41188484/jest-typescript-ts-jest-coverage-is-slightly-incorrect/43119810
 describe('private key factory', () => {
@@ -22,13 +23,27 @@ describe('private key factory', () => {
     const TEST_KEY = '9b4b3da7d2d1c8749743a4ac17c151405f7900832bb4cf88735721cef4627096';
 
     //Test the Private key value object
-    test('PrivateKey test', () => {
+    test('private key test getter', () => {
 
         const key:PrivateKey = new PrivateKey(new Buffer(TEST_KEY, 'hex'));
 
         expect(key.getPrivKeyBuffer()).toEqual(new Buffer(TEST_KEY, 'hex'));
 
         expect(key.getPrivKey()).toEqual(TEST_KEY);
+
+    });
+
+    test('throw error if given private key is an invalid private key', async () => {
+
+        let error:any;
+
+        try {
+            new PrivateKey(new Buffer('i_am_trolling_you'))
+        }catch (e) {
+            error = e;
+        }
+
+        expect(error).toBeInstanceOf(InvalidPrivateKey);
 
     });
 
